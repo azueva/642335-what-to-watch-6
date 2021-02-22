e1f1b255af7afb3b47ca1ec652b9d3d3614f7cb8
azueva <75371924+azueva@users.noreply.github.com>
2021-02-22 19:10:34 +0300

    Частично реализует функциональность просмотра фильма на отд. экране

--- src/components/pages/player/player.jsx
+++ src/components/pages/player/player.jsx
@@ -1,25 +1,88 @@
-import React from "react";
+/* eslint-disable no-console */
+import React, {useState, useEffect} from "react";
 import {useHistory} from "react-router-dom";
-
+import VideoPlayer from "../../video-player/video-player";
 import PropTypes from 'prop-types';
 import {formatMinToTimeString} from '../../../utils';
 
+const POSTER = `img/player-poster.jpg`;
+const VIDEO_LINK = `media/sintel_trailer-480p.mp4`;
+
+
+const PlayerStatusName = {
+  false: `STOP`,
+  true: `PLAY`,
+  undefined: `???`,
+};
+
+const PlayButtonState = {
+  PLAY: true,
+  PAUSE: false,
+};
+
 const Player = (props) => {
-  const {name, videoLink, runTime} = props;
+  const {id, name, /* videoLink, */ runTime} = props;
+  const [playerStatus, setPlayerStatus] = useState(null);
+  const [playButtonState, setPlayButtonState] = useState(PlayButtonState.PLAY);
+  const [fullScreenButtonClick, setFullScreenButtonClick] = useState(null);
   const history = useHistory();
+
+  /* componentDidMount */
+  useEffect(() => {
+    console.log(`Play${id} ... ${name} componentDidMount`);
+  }, []);
+
 
   const handleExitBtnClick = () => {
     history.goBack();
   };
 
+  const handlePlayerStatusChange = (status) => {
+    setPlayerStatus(PlayerStatusName[status]);
+    console.log(`Play${id}. ${playerStatus} => ${PlayerStatusName[status]}`);
+  };
+
+
+  // ⛔❔❕ Передаю событие клика в дочерний компонент (VideoPlayer) через пропс
+  //  ...  как-то страшно выглядит
+  const handleFullScreenButtonClick = () => {
+    console.log(`Play${id}. handleFullScreenButtonClick`);
+    setFullScreenButtonClick((prev) => !prev);
+  };
+
+  // ⚡❕ здесь ошибка, не переключает состояние кнопки PLAY/PAUSE
+  // ⚡❕ если оно изменилось в полноэкранном режиме
+  const handlePlayButtonClick = () => {
+    console.log(`Play${id}. handlePlayButtonClick ${setPlayButtonState} => PAUSE}`);
+    setPlayButtonState(PlayButtonState.PAUSE);
+  };
+
+  const handlePauseButtonClick = () => {
+    console.log(`Play${id}. handlePauseButtonClick ${setPlayButtonState} => PLAY}`);
+    setPlayButtonState(PlayButtonState.PLAY);
+  };
+
   return (
-    <div className="player">
-      <video src={videoLink} className="player__video" poster="img/player-poster.jpg"></video>
+    <div className="player"
+      style={{
+        backgroundColor: `darkgrey`
+      }}
+    >
+      <VideoPlayer
+        id={id}
+        src={VIDEO_LINK}
+        poster={POSTER}
+        onPlayerStatus={handlePlayerStatusChange}
+        playButtonState={!playButtonState}
+        fullScreenButtonClick={fullScreenButtonClick}
+        style={{
+          objectFit: `contain`,
+        }}
+      />
 
       <button type="button" className="player__exit"
         onClick={handleExitBtnClick}
-      >
-        Exit
+      >Exit
       </button>
 
       <div className="player__controls">
@@ -32,17 +95,33 @@ const Player = (props) => {
         </div>
 
         <div className="player__controls-row">
-          <button type="button" className="player__play">
-            <svg viewBox="0 0 19 19" width="19" height="19">
-              <use xlinkHref="#play-s"></use>
-            </svg>
-            <span>Play</span>
-          </button>
+          { playButtonState ?
+            <button type="button" className="player__play"
+              onClick={handlePlayButtonClick}
+            >
+              <svg viewBox="0 0 19 19" width="19" height="19">
+                <use xlinkHref="#play-s"></use>
+              </svg>
+              <span>Play</span>
+            </button>
+            :
+            <button type="button" className="player__play"
+              onClick={handlePauseButtonClick}
+            >
+              <svg viewBox="0 0 14 21" width="14" height="21">
+                <use xlinkHref="#pause"></use>
+              </svg>
+              <span>Pause</span>
+            </button>
+          }
+
           <div className="player__name">
             {name}
           </div>
 
-          <button type="button" className="player__full-screen">
+          <button type="button" className="player__full-screen"
+            onClick={handleFullScreenButtonClick}
+          >
             <svg viewBox="0 0 27 27" width="27" height="27">
               <use xlinkHref="#full-screen"></use>
             </svg>
@@ -55,8 +134,9 @@ const Player = (props) => {
 };
 
 Player.propTypes = {
+  id: PropTypes.number,
   name: PropTypes.string.isRequired,
-  videoLink: PropTypes.string.isRequired,
+  // videoLink: PropTypes.string.isRequired,
   runTime: PropTypes.number.isRequired,
 };
 
--- src/components/video-player/video-player.jsx
+++ src/components/video-player/video-player.jsx
@@ -5,11 +5,13 @@ import PropTypes from 'prop-types';
 
 const VideoPlayer = (props) => {
   const {id, src, poster, onPlayerStatus} = props;
-  const {isMuted = true, isControls = false, isAutoPlay = false} = props;
+  const {playButtonState = false, fullScreenButtonClick = null} = props;
+  const {isMuted = false, isAutoPlay = false} = props;
   const {width = `100%`, height = `100%`, style = {}} = props;
 
   const [isReady, setIsReady] = useState(false);
   const [isCurrentlyPlaying, setIsCurrentlyPlaying] = useState(undefined);
+  const [isCurrentlyFullScreen, setIsCurrentlyFullScreen] = useState(undefined);
 
   const playerRef = useRef();
 
@@ -30,31 +32,79 @@ const VideoPlayer = (props) => {
 
     playerRef.current.onpause = () => {
       console.log(`${id}.onpause`);
-      // debugger;
       setIsCurrentlyPlaying(false);
     };
     console.groupEnd();
   }, []);
-
 
   /* componentDidUpdate */
   useEffect(() => {
     console.group(`${id}.componentDidUpdate - isReady`);
     console.log(`isReady ${isReady}`);
     console.log(playerRef.current.src);
-    // debugger;
     if (isReady & isAutoPlay) {
       playerRef.current.play();
     } else {
       playerRef.current.pause();
     }
     console.groupEnd();
+  // ⚡❕ возможна ошибка, нужен ещё и isAutoPlay в массиве?
   }, [isReady]);
 
   useEffect(() => {
+    //
+    // ⛔❔❕ может быть, тут нужен useCallback?
+    //
     console.log(`${id}.isCurrentlyPlaying ${isCurrentlyPlaying}`);
     onPlayerStatus(isCurrentlyPlaying);
   }, [isCurrentlyPlaying]);
+
+  useEffect(() => {
+    console.group(`${id}.componentDidUpdate - playButtonStatus`);
+    console.log(`playButtonStatus ${playButtonState}`);
+    if (playButtonState) {
+      playerRef.current.play();
+    } else {
+      playerRef.current.pause();
+    }
+    console.groupEnd();
+  }, [playButtonState]);
+
+
+  useEffect(() => {
+    //
+    // ⛔❔❕ fullScreenButtonState === null как признак первой отрисовки
+    //  ...  как-то плохо выглядит
+    //
+    console.log(`${id}.fullScreenButtonClick ${fullScreenButtonClick}`);
+    if (!document.fullscreenElement && fullScreenButtonClick !== null) {
+      setIsCurrentlyFullScreen(true);
+    }
+  }, [fullScreenButtonClick]);
+
+  const handleFullScreenChange = () => {
+    if (document.fullscreenElement) {
+      console.log(`Entered full-screen mode.`);
+    } else {
+      console.log(`Leaving full-screen mode.`);
+      document.removeEventListener(`fullscreenchange`, handleFullScreenChange);
+      setIsCurrentlyFullScreen(false);
+    }
+  };
+
+  useEffect(() => {
+    console.log(`${id}.isCurrentlyFullScreen  => ${isCurrentlyFullScreen}`);
+    if (isCurrentlyFullScreen && !document.fullscreenElement) {
+      playerRef.current.requestFullscreen()
+      .then(() => {
+        console.log(`${id}. Toggle Player to full-screen mode on`);
+        document.addEventListener(`fullscreenchange`, handleFullScreenChange);
+      })
+      .catch((err) => {
+        console.warn(`Error attempting to enable full-screen mode: ${err.message} (${err.name})`);
+      });
+    }
+  }, [isCurrentlyFullScreen]);
 
   useEffect(() => {
     /* componentWillUnmount */
@@ -75,7 +125,6 @@ const VideoPlayer = (props) => {
         id={id}
         type="video/mp4"
         autoPlay={isAutoPlay}
-        controls={isControls}
         src={src}
         muted={isMuted}
         ref={playerRef}
@@ -84,7 +133,7 @@ const VideoPlayer = (props) => {
         height={height}
         style={style}
       >
-        Your browser doesn&apos;t support videos,
+        Your browser doesn&apos;t support HTML5 videos,
       </video>
     </React.Fragment>
   );
@@ -94,8 +143,9 @@ const VideoPlayer = (props) => {
 VideoPlayer.propTypes = {
   id: PropTypes.number,
   isMuted: PropTypes.bool,
-  isControls: PropTypes.bool,
   isAutoPlay: PropTypes.bool,
+  playButtonState: PropTypes.bool,
+  fullScreenButtonClick: PropTypes.oneOf([true, false, null]),
   src: PropTypes.string.isRequired,
   poster: PropTypes.string.isRequired,
   width: PropTypes.string,

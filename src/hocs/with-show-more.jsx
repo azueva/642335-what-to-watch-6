ec5c5c287d46fd947df094c7cfd47b96936faaa8
azueva <75371924+azueva@users.noreply.github.com>
2021-02-28 01:04:29 +0300

    Заменяет анонимную функцию в HOC именованной

--- src/hocs/with-show-more.jsx
+++ src/hocs/with-show-more.jsx
@@ -1,35 +1,34 @@
 import React, {useState} from 'react';
-import PropTypes from "prop-types";
 // import MovieProp from '../components/props/movie.prop';
 import ShowMore from "../components/blocks/show-more/show-more";
 
-export const withShowMore = (Component) => (props) => {
-  const {films, listSize} = props;
-  const [shownItems, setShownItems] = useState(listSize);
+export const withShowMore = (Component) => {
 
-  const handleShowMoreClick = () => setShownItems((prevShownItem) =>
-    prevShownItem + listSize
-  );
+  const addShowNore = (propsParameters) => {
+    const {films, listSize} = propsParameters;
+    const [shownItems, setShownItems] = useState(listSize);
 
-  return (
-    <React.Fragment>
+    const handleShowMoreClick = () => setShownItems((prevShownItem) =>
+      prevShownItem + listSize
+    );
 
-      <Component
-        {...props}
-        films={films.slice(0, shownItems)}
-        listSize={shownItems}
-      >
-      </ Component>
+    return (
+      <React.Fragment>
 
-      {shownItems < films.length && <ShowMore onClick={handleShowMoreClick} />}
+        <Component
+          {...propsParameters}
+          films={films.slice(0, shownItems)}
+          listSize={shownItems}
+        >
+        </ Component>
 
-    </React.Fragment>
-  );
+        {shownItems < films.length && <ShowMore onClick={handleShowMoreClick} />}
+
+      </React.Fragment>
+    );
+  };
+
+  return addShowNore;
 };
-
-// withShowMore.propTypes = {
-//   films: PropTypes.arrayOf(MovieProp),
-//   listSize: PropTypes.number.isRequired,
-// };
 
 export default withShowMore;
